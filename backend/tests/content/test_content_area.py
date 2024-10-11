17fe8ca747386f3c101e21f54e6b34cf7e8fedf1
Claudio Weiler <claudioweiler@users.noreply.github.com>
2024-10-11 13:24:32 -0300

    Exercício 22: Testes de permissionamento

--- backend/tests/content/test_content_area.py
+++ backend/tests/content/test_content_area.py
@@ -1,3 +1,4 @@
+from AccessControl import Unauthorized
 from plone import api
 from plone.dexterity.fti import DexterityFTI
 from trepr.intranet.content.area import Area
@@ -53,8 +54,29 @@ class TestArea:
     def test_has_behavior(self, get_behaviors, behavior):
         assert behavior in get_behaviors(CONTENT_TYPE)
 
-    def test_create(self, area_payload):
-        with api.env.adopt_roles(["Manager"]):
+    @pytest.mark.parametrize(
+        "role",
+        [
+            "Manager",
+            "Site Administrator",
+        ],
+    )
+    def test_can_create(self, area_payload, role):
+        with api.env.adopt_roles(role):
             content = api.content.create(container=self.portal, **area_payload)
         assert content.portal_type == CONTENT_TYPE
         assert isinstance(content, Area)
+
+    @pytest.mark.parametrize(
+        "role",
+        [
+            "Editor",
+            "Reviewer",
+            "Contributor",
+            "Reader",
+        ],
+    )
+    def test_cant_create(self, area_payload, role):
+        with pytest.raises(Unauthorized):
+            with api.env.adopt_roles(role):
+                api.content.create(container=self.portal, **area_payload)
