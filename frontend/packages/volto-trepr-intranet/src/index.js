fbe05b50217f0f13e9afab2b0c7451005ce81fcd
Claudio Weiler <claudioweiler@users.noreply.github.com>
2024-10-17 15:46:03 -0300

    Exercício 45: Criar um novo bloco Volto, para exibir a previsão do tempo

--- frontend/packages/volto-trepr-intranet/src/index.js
+++ frontend/packages/volto-trepr-intranet/src/index.js
@@ -1,9 +1,15 @@
+import { cloneDeep } from 'lodash';
 // Views
 import AreaView from './components/Views/AreaView';
 import PessoaView from './components/Views/PessoaView';
 import AreaGridItem from './components/Blocks/Grid/AreaGridItem';
 /// Listing
 import IconListingTemplate from './components/Blocks/Listing/IconListingTemplate';
+// Blocks
+/// Clima
+import ClimaEdit from './components/Blocks/Clima/Edit';
+import ClimaView from './components/Blocks/Clima/View';
+import climaSVG from '@plone/volto/icons/cloud.svg';
 
 const applyConfig = (config) => {
   config.settings = {
@@ -53,6 +59,41 @@ const applyConfig = (config) => {
     },
   ];
 
+  // Clima
+  config.blocks.blocksConfig.climaBlock = {
+    id: 'climaBlock',
+    title: 'Clima',
+    group: 'common',
+    icon: climaSVG,
+    view: ClimaView,
+    edit: ClimaEdit,
+    restricted: false,
+    mostUsed: true,
+    sidebarTab: 1,
+    blockHasOwnFocusManagement: false,
+  };
+
+  // Adiciona blocos ao Grid
+  const localBlocks = ['climaBlock'];
+
+  // Add Blocks to gridBlock
+  // It's important to maintain the chain, and do not introduce pass by reference in
+  // the internal `blocksConfig` object, so we clone the object to avoid this.
+  ['gridBlock'].forEach((blockId) => {
+    const block = config.blocks.blocksConfig[blockId];
+    if (
+      block !== undefined &&
+      block.allowedBlocks !== undefined &&
+      block.blocksConfig !== undefined
+    ) {
+      block.allowedBlocks = [...block.allowedBlocks, ...localBlocks];
+      localBlocks.forEach((blockId) => {
+        block.blocksConfig[blockId] = cloneDeep(
+          config.blocks.blocksConfig[blockId],
+        );
+      });
+    }
+  });
   return config;
 };
 
